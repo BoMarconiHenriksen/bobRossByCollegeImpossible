b9559d5d1e23d24bbed731c41ad11695630ae5a6
Bo Henriksen <bomarconi@gmail.com>
2018-09-18 15:09:26 +0200

    Henter tekst filen, og lægger den i et dictionary.

---
+++ get_bob_ross.py
@@ -0,0 +1,58 @@
+"""
+Beskrivelse her
+
+Usage:
+    python downloader.py [<url>] [<file_name>]
+"""
+
+import os
+import sys
+from urllib import request as req
+
+file_name = None
+
+
+def download(from_url, to_file):
+    if not os.path.isfile(to_file):
+        req.urlretrieve(from_url, to_file)
+
+
+if __name__ == '__main__':
+    try:
+
+        _, url, file_name = sys.argv
+    except:
+        try:
+            _, url = sys.argv
+            file_name = os.path.basename(url)
+        except:
+            try:
+
+                # open file
+                cfg_file = 'list_of_files.txt'
+                with open(cfg_file) as fp:
+                    for line in fp:
+                        file_name = os.path.basename(line.rstrip())
+                        url = line
+                        download(url, file_name)
+                sys.exit(0)
+            except Exception as e:
+                print(__doc__)
+                sys.exit(1)
+    download(url, file_name)
+
+
+def convert_file_dict():
+    #bob_ross_dict = {}
+    with open("BobRoss.txt", encoding='utf8') as fp:  # , "r"
+        bob_ross_dict = {key: value for key, value in [
+            line.split(None, 1) for line in fp]}
+        # De 2 linjer oven over erstatter nedenstående linjer.
+        # for line in fp:
+        #   key, value = line.strip().split(None, 1)
+        #  bob_ross_dict[key] = value
+    # print(bob_ross_dict)
+    print(list(bob_ross_dict.items())[1])
+
+
+convert_file_dict()
