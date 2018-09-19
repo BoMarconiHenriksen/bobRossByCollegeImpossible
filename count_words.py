856dcf4ab13179a6993c1958a09a4a74261c682d
Bo Henriksen <bomarconi@gmail.com>
2018-09-19 15:21:03 +0200

    Key er en del af arrayet.

--- count_words.py
+++ count_words.py
@@ -3,11 +3,11 @@ import re
 
 
 def count_frequency_words(dict):
-    a = Counter.(dict.values)
-    #split_values = {key: value.strip().split(None, 50)
-     #               for key, value in dict.items()}
 
-    #return print(split_values)
+    split_values = {key: value.strip().split(None, 50)
+                    for key, value in dict.items()}
+
+    return print(split_values)
 
     #word_frequency = Counter(split_values.values())
     # return print(word_frequency)
