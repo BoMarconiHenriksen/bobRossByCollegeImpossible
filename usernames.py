3a178dbffcd25d82019d19d17cad67821d233c5d
Bo Henriksen <bomarconi@gmail.com>
2018-09-20 09:09:40 +0200

    Refactoring i usernames.py

--- usernames.py
+++ usernames.py
@@ -1,19 +1,18 @@
-# 2018-09-18 kl. 08.29 AM --  Christian Lykke -- usernames
+""" Hvor mange forskellige brugere er der i BobRussel.txt? """
 
 import re
-
-def get_default_line(bob_ross_dict):
-    return list(bob_ross_dict.items())[1]
 
 
 def get_username_count(bob_ross_dict):
     nameDict = {}
     names_in_line = []
-    
+
     for line in list(bob_ross_dict.items()):
+        # Reg ex, der finder brugeren i hver value.
         names_in_line = re.findall(', [\'\"](.*?): ', str(line))
-        if len(names_in_line)>0:
+        if len(names_in_line) > 0:
+            # HVAD GØR DENNE HER LINJE OG NÆSTE?
             nameDict.setdefault(names_in_line[0], 0)
             nameDict[names_in_line[0]] += 1
-       
-    return len(nameDict)
+
+    return print(f'Der er {len(nameDict)} forskellige brugere i BobRussel.txt.')
