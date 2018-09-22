257a7382fde76d2f34d93b8b3fba715695522d95
NisseAnd <anders-nissen@stofanet.dk>
2018-09-22 23:14:49 +0200

    Tilføjede kommentarer til koden i ruined_word.py for læsevenlighed

--- ruined_word.py
+++ ruined_word.py
@@ -1,10 +1,13 @@
+"""
+    Funktionen tæller, hvor mange gange ordet ruined (i diverse former) forekommer i datasættet.
+"""
 def ruin_word():
-    inputData = open('BobRoss.txt', encoding='utf8').readlines()
+    inputData = open('BobRoss.txt', encoding='utf8').readlines() #Bruger open() metoden til, at åbne txt filen direkte og læser derefter, hver linje
     counter = 0
-    for line in inputData:
-        if ("Ruined" in line) or ("ruined" in line) or ("RUINED" in line):
+    for line in inputData: #Itererer over txt filen som inputData
+        if ("Ruined" in line) or ("ruined" in line) or ("RUINED" in line): #Tæller forekomster af ordet ruined (i diverse former)
             counter += 1
-    print(f'The text file write RUINED, Ruined and ruined {counter} times.')
+    print(f'The text file write RUINED, Ruined and ruined {counter} times.') #Printer antallet af ruined
 
 
 #ruin_work()
